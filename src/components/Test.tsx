981a91dd4951f92a582c82c0ffc6f7362c1dbb62
kobayashi <kobayashi_hiromu@moltsinc.co.jp>
2025-05-18 08:46:18 +0900

    first

---
+++ src/components/Test.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+
+export default function Test() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const postData = async () => {
+    axios.get("http://localhost:8080/sanctum/csrf-cookie").then((res: any) => {
+      console.log(res);
+    });
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 m-3 max-w-sm"
+        placeholder="email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="text"
+        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 m-3 max-w-sm"
+        placeholder="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <div>
+        <button
+          className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded m-3"
+          onClick={() => {
+            alert("送信してもよろしいですか？");
+            postData();
+          }}
+        >
+          送信
+        </button>
+      </div>
+    </div>
+  );
+}
