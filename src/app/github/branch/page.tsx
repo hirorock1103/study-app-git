ad1ed59101e61ac23682441df32882a2a8548324
kobayashi <kobayashi_hiromu@moltsinc.co.jp>
2025-05-27 12:17:07 +0900

    コミット一覧

--- src/app/github/branch/page.tsx
+++ src/app/github/branch/page.tsx
@@ -1,5 +1,15 @@
+"use client";
 import GithubBranchDetail from "@/components/GithubBranchDetail";
+import { useSearchParams } from "next/navigation";
 
 export default function GithubBranchPage() {
-  return <GithubBranchDetail />;
+  const searchParams = useSearchParams();
+  const branchName = searchParams.get("branch_name") || "";
+  const repositoryName = searchParams.get("repository_name");
+  return (
+    <GithubBranchDetail
+      branchName={branchName}
+      repositoryName={repositoryName || ""}
+    />
+  );
 }
--- src/components/GithubBranchDetail.tsx
+++ src/components/GithubBranchDetail.tsx
@@ -1,5 +1,73 @@
-import React from "react";
+"use client";
 
-export default function GithubBranchDetail() {
-  return <div>GithubBranchDetail</div>;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Commit {
+  id: number;
+  branchName: string;
+  sha: string;
+  repositoryId: number;
+  userId: number;
+  commitName: string;
+  commitEmail: string;
+  commitDate: string;
+  commitMessage: string;
+  commitUrl: string;
+  files: string;
+}
+
+export default function GithubBranchDetail({
+  branchName,
+  repositoryName,
+}: {
+  branchName: string;
+  repositoryName: string;
+}) {
+  const [commitHistory, setCommitHistory] = useState<Commit[]>([]);
+
+  const handleFetchCommits = async () => {
+    const baseUrl = "http://localhost:8080";
+    const response = await axios.get(
+      `${baseUrl}/api/user/github/db/commits?repository_name=${repositoryName}&branch_name=${branchName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    console.log(response.data.data);
+    setCommitHistory(response.data.data);
+  };
+
+  useEffect(() => {
+    handleFetchCommits();
+  }, [branchName, repositoryName]);
+
+  return (
+    <>
+      {/* list commits list */}
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1>Commits</h1>
+        <ul className="list-disc list-inside ml-4">
+          {commitHistory.map((commit) => (
+            <li
+              key={commit.id}
+              className="border-b border-gray-300 py-2 space-y-2 flex justify-between"
+            >
+              <div className="flex flex-col">
+                <p className="text-sm text-gray-500">
+                  {commit.commitDate.split("T")[0]}
+                </p>
+                <p>{commit.commitMessage}</p>
+              </div>
+              <button className="bg-blue-500 text-white px-4 py-1 rounded-md text-xs hover:bg-blue-600">
+                開閉式
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
